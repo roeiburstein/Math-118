660f042008807df51df0a898f5a6a7b0b5abc123
roeiburstein <roei.burstein@gmail.com>
2020-03-02 18:10:23 -0800

    Add bottom-up fibonacci and modify tester

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,10 +1,9 @@
 import time
 
-MAX_FIB_INDEX = 100
+MAX_FIB_INDEX = 501
 
 def main():
     testing([10, 20, 30])
-
 
 def fib_bf(index):
     if(index == 0 or index == 1):
@@ -16,15 +15,20 @@ list = [-1] * MAX_FIB_INDEX
 list[0] = 0
 list[1] = 1
 
-def fib_dp(index):
+def fib_dp_top_down(index):
     if(list[index] is not -1):
         return list[index]
     else:
-        list[index] = fib_dp(index - 1) + fib_dp(index - 2)
+        list[index] = fib_dp_top_down(index - 1) + fib_dp_top_down(index - 2)
         return list[index]
 
-def testing(testing_nums):
+def fib_dp_bottom_up(index):
+    my_list = [0, 1]
+    for iter in range(2, index + 1):
+        my_list.append(my_list[iter - 1] + my_list[iter - 2])
+    return my_list[index]
 
+def testing(testing_nums):
     for num in testing_nums:
         print("\nBrute Force Fibbonacci (", num, "): ")
         start = time.time()
@@ -32,9 +36,15 @@ def testing(testing_nums):
         end = time.time()
         print("Elapsed Time: ", end - start, " seconds")
 
-        print("\nDynamic Programming Fibbonacci (", num, "): ")
+        print("\nTop Down Dynamic Programming Fibbonacci (", num, "): ")
         start = time.time()
-        fib_dp(num)
+        fib_dp_top_down(num)
+        end = time.time()
+        print("Elapsed Time: ", end - start, " seconds")
+
+        print("\nBottom Up Dynamic Programming Fibbonacci (", num, "): ")
+        start = time.time()
+        fib_dp_bottom_up(num)
         end = time.time()
         print("Elapsed Time: ", end - start, " seconds")
 
