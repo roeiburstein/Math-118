735667227e2b678192bea4582a1271eca6fc913c
roeiburstein <roei.burstein@gmail.com>
2020-03-02 18:17:02 -0800

    Add comments

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,20 +1,22 @@
 import time
 
-MAX_FIB_INDEX = 501
+MAX_FIB_INDEX = 501 # Used for array with -1's for top-down dp
 
 def main():
     testing([10, 20, 30])
 
+# Generic fibonacci recursive function: order O(n^2)
 def fib_bf(index):
     if(index == 0 or index == 1):
-        return index
+        return index # fib(0) = 0 and fib(1) = 1, so just return index
     else:
-        return fib_bf(index - 1) + fib_bf(index - 2)
+        return fib_bf(index - 1) + fib_bf(index - 2) # fib(n) = fib(n-1) + fib(n-2)
 
-list = [-1] * MAX_FIB_INDEX
+list = [-1] * MAX_FIB_INDEX # Creates list with MAX_FIB_INDEX number of -1's
 list[0] = 0
 list[1] = 1
 
+# DP Fibonacci starting from target index and going backward
 def fib_dp_top_down(index):
     if(list[index] is not -1):
         return list[index]
@@ -22,12 +24,14 @@ def fib_dp_top_down(index):
         list[index] = fib_dp_top_down(index - 1) + fib_dp_top_down(index - 2)
         return list[index]
 
+# DP Fibonacci starting from index 2 (fib(0)=0 and fib(1)=1) to target index
 def fib_dp_bottom_up(index):
     my_list = [0, 1]
-    for iter in range(2, index + 1):
-        my_list.append(my_list[iter - 1] + my_list[iter - 2])
+    for iter in range(2, index + 1): # fib(0) and fib(1) are predefined
+        my_list.append(my_list[iter - 1] + my_list[iter - 2]) # Add fib(iter) to the list
     return my_list[index]
 
+# Tester for each type of fibonacci with associated elapsed times:
 def testing(testing_nums):
     for num in testing_nums:
         print("\nBrute Force Fibbonacci (", num, "): ")
