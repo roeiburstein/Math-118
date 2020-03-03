7679fce7c727807d817d2353d4fc7bb3f143b83f
roeiburstein <roei.burstein@gmail.com>
2020-03-03 12:34:14 -0800

    Add Ly=b (halfway done with solving linear system)

--- LU_Decomposition.py
+++ LU_Decomposition.py
@@ -1,8 +1,15 @@
 import numpy as np
 
 def main():
-   test_1()
+   # test_1()
    # test_2()
+   A = np.array([[2, 4, -4],
+                 [1, -4, 3],
+                 [-6, -9, 10]], np.float64)
+
+   b = np.array([12, -21, -24])
+
+   linear_system(A, b)
 
 def decompose(matrix):
    dim = len(matrix)
@@ -22,6 +29,24 @@ def decompose(matrix):
          L[count, num] = (-1 * multiplier)
    return L, matrix
 
+
+def linear_system(A, b):
+   L, U = decompose(A)
+   y = np.zeros(len(A), np.float)
+
+   for row in range(len(A)):
+      sum = b[row]
+      for col in range(len(A)):
+         if col != row:
+            sum -= L[row][col] * y[col]
+      y[row] = sum
+
+   x = np.zeros(len(A), np.float)
+
+   #LUx=b
+   #Ly=b
+   #Ux=y
+
 def test_1():
    dim = 3
    A = np.random.rand(dim, dim)
@@ -31,16 +56,12 @@ def test_1():
    print("\nL*U: \n", np.dot(L, U))
 
 def test_2():
-   B = np.array([[4, 1, 7, 9], [1, 6, 3, 1], [4, 8, 9, 9], [12, 6, 1, 9]], np.float64)
+   B = np.array([[4, 1, 7, 9], [1, 6, 3, 1], [4, 8, 9, 9], [12, 6, 1, 9]],
+                np.float64)
    print("B: \n", B)
    L, U = decompose(B)
    print("\nL: \n", L, "\nU: \n", U)
    print("\nL*U: \n", np.dot(L, U))
 
-def linear_system(A, b):
-   L, U = decompose(A)
-   #LUx=b
-   #Ux=y
-   #Ly=b
 if __name__ == "__main__":
     main()
