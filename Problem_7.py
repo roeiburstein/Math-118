d75d00d84b41ddc199ea1f45ed28fff1e6787f1c
roeiburstein <roei.burstein@gmail.com>
2020-03-13 22:42:11 -0700

    Finish problem 7

--- Problem_7.py
+++ Problem_7.py
@@ -7,16 +7,17 @@ P = np.array([[0, 1, 1/2, 1/3],
 P_tilda = np.array([[0.0125, 0.9625, 0.4875, 0.329166],
                     [0.9625, 0.0125, 0.0125, 0.329166],
                     [0.0125, 0.0125, 0.0125, 0.329166],
-                    [0.0125, 0.0125, 0.4875, 0.012]])
-#print(np.sum(P_tilda,axis=0))
+                    [0.0125, 0.0125, 0.4875, 0.0125]])
 def main():
-   NUM_PRODUCTS = 20
+   NUM_PRODUCTS = 10000
    v = np.random.rand(4, 1)
-   #v = np.array([1, 2, 3, 4])
    for i in range(NUM_PRODUCTS):
       v = np.matmul(P_tilda, v)
+   two_norm = np.linalg.norm(v)
+   for j, i in enumerate(v):
+      v[j] = (i / two_norm)
    print(v)
-   print("\n\n\n", np.matmul(P_tilda, v))
-
+   print("\n\n", np.matmul(P_tilda, v))
+   print("\n\n", np.linalg.eig(P_tilda)[1])
 if __name__ == "__main__":
     main()
